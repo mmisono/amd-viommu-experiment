e47c61994acac44d0b61251c1733b432fafc77b4
Masanori Misono <m.misono760@gmail.com>
2022-04-08 15:49:30 +0200

    update config

--- cloudlab/setup_disk.sh
+++ cloudlab/setup_disk.sh
@@ -12,21 +12,26 @@
 #     sudo mv /usr/local/etc/emulab/watchdog /usr/local/etc/emulab/watchdog-disabled
 # fi
 
-
 # Mount extra space, if haven't already
 if [ ! -d /mnt/extra ]; then
     sudo mkdir -p /mnt/extra
 
-    if [ -e /usr/testbed/bin/mkextrafs ] && [ -b /dev/sdb ]; then
-        if [ ! -b /dev/sdb1 ]; then
-            # Sometimes this command fails on the first try
-            sudo /usr/testbed/bin/mkextrafs -s 1 -r /dev/sdb -qf "/mnt/extra/" || sudo /usr/testbed/bin/mkextrafs -s 1 -r /dev/sdb -qf "/mnt/extra/"
-        else
-            echo "/dev/sdb1 /mnt/extra/ ext4 defaults 0 0" | sudo tee -a /etc/fstab
+    if [ -e /usr/testbed/bin/mkextrafs ]; then
+        if [ -b /dev/sdb ]; then
+            if [ ! -b /dev/sdb1 ]; then
+                # Sometimes this command fails on the first try
+                sudo /usr/testbed/bin/mkextrafs -s 1 -r /dev/sdb -qf "/mnt/extra/" || sudo /usr/testbed/bin/mkextrafs -s 1 -r /dev/sdb -qf "/mnt/extra/"
+            else
+                echo "/dev/sdb1 /mnt/extra/ ext4 defaults 0 0" | sudo tee -a /etc/fstab
+            fi
+            sudo mount -a
+            mount | grep sdb || (echo "ERROR: mount failed, exiting!" && exit 1)
+        elif [ -b /dev/nvme0n1p4 ]; then
+            sudo mkfs.ext4 /dev/nvme0n1p4
+            echo "/dev/nvme0n1p4 /mnt/extra/ ext4 defaults 0 0" | sudo tee -a /etc/fstab
+            sudo mount -a
+            mount | grep nvme || (echo "ERROR: mount failed, exiting!" && exit 1)
         fi
-        sudo mount -a
-        # Check that the mount succeeded (sometimes mkextrafs succeeds but device not mounted)
-        mount | grep sdb || (echo "ERROR: mount failed, exiting!" && exit 1)
     else
         echo "No disk found!" && exit 1
     fi
